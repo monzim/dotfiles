fe4d79b433e9f26ce53b9c7002b908621c385c97
Azraf Al Monzim <dev@monzim.com>
2024-02-02 11:22:44 +0600

    Add image.nvim plugin and enable git status and diagnostics in neo-tree

--- nvim/.config/nvim/lua/custom/plugins/filetree.lua
+++ nvim/.config/nvim/lua/custom/plugins/filetree.lua
@@ -5,10 +5,22 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     version = "*",
     dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim"},
+    "MunifTanjim/nui.nvim", "3rd/image.nvim" -- Optional image support in preview window: See `# Preview Mode` for more information,
+    },
+
     config = function()
         require('neo-tree').setup {
             popup_border_style = "rounded",
+            enable_git_status = true,
+            enable_diagnostics = true,
+            group_empty_dirs = false, -- when true, empty folders will be grouped together
+            -- hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+            -- in whatever position is specified in window.position
+            -- "open_current",  -- netrw disabled, opening a directory opens within the
+            -- window like netrw would, regardless of window.position
+            -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+            -- use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+
             window = {
                 position = "right",
                 width = 40,
