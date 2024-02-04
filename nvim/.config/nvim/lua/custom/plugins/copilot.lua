91afac086cb1825dab5460aa73f66339222b3b6c
Azraf Al Monzim <dev@monzim.com>
2024-02-05 00:35:28 +0600

    Update copilot plugins and configurations

--- nvim/.config/nvim/lua/custom/plugins/copilot.lua
+++ nvim/.config/nvim/lua/custom/plugins/copilot.lua
@@ -1,8 +1,67 @@
-vim.g.copilot_assume_mapped = true
-
+-- vim.g.copilot_assume_mapped = true
+-- return {
+--     "github/copilot.vim",
+--     opt = true,
+--     cmd = {"Copilot"}
+-- }
 return {
-    "github/copilot.vim",
-    opt = true,
-    cmd = {"Copilot"}
-}
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+        require('copilot').setup({
+            panel = {
+                enabled = true,
+                auto_refresh = false,
+                keymap = {
+                    jump_prev = "[[",
+                    jump_next = "]]",
+                    accept = "<CR>",
+                    refresh = "gr",
+                    open = "<M-CR>"
+                },
+                layout = {
+                    position = "right", -- | top | left | right
+                    ratio = 0.4
+                }
+            },
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                debounce = 75,
+                keymap = {
+                    accept = false,
+                    accept_word = false,
+                    accept_line = false,
+                    next = "<M-]>",
+                    prev = "<M-[>",
+                    dismiss = "<C-]>"
+                }
+            },
+            filetypes = {
+                yaml = false,
+                markdown = true,
+                help = false,
+                gitcommit = true,
+                gitrebase = false,
+                hgcommit = false,
+                svn = false,
+                cvs = false,
+                ["."] = false
+            },
+            copilot_node_command = 'node',
+            server_opts_overrides = {}
+        })
 
+        vim.keymap.set("i", "<Tab>", function()
+            if require("copilot.suggestion").is_visible() then
+                require("copilot.suggestion").accept()
+            else
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+            end
+        end, {
+            desc = "Super Tab"
+        })
+
+    end
+}
--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -156,6 +156,7 @@ require('lazy').setup({ -- NOTE: First, some plugins that don't require any conf
 }, {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
+    dependencies = {'AndreM222/copilot-lualine'}, -- my addition
     -- See `:help lualine.txt`
     opts = {
         options = {
@@ -163,7 +164,58 @@ require('lazy').setup({ -- NOTE: First, some plugins that don't require any conf
             theme = 'auto',
             component_separators = '|',
             section_separators = ''
-        }
+        },
+        sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch', 'diff', {
+                'diagnostics',
+                sources = {"nvim_diagnostic"},
+                symbols = {
+                    error = ' ',
+                    warn = ' ',
+                    info = ' ',
+                    hint = ' '
+                }
+            }},
+            lualine_c = {'filename'},
+            lualine_x = {{
+                'copilot',
+                -- Default values
+                symbols = {
+                    status = {
+                        icons = {
+                            enabled = " ",
+                            sleep = " ", -- auto-trigger disabled
+                            disabled = " ",
+                            warning = " ",
+                            unknown = " "
+                        },
+                        hl = {
+                            enabled = "#50FA7B",
+                            sleep = "#AEB7D0",
+                            disabled = "#6272A4",
+                            warning = "#FFB86C",
+                            unknown = "#FF5555"
+                        }
+                    }
+                },
+                show_colors = false,
+                show_loading = true
+            }, 'encoding', 'fileformat', 'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'}
+        },
+
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+        },
+        tabline = {},
+        extensions = {}
     }
 }, {
     -- Add indentation guides even on blank lines
