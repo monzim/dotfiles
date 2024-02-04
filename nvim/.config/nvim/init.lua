21a7b820208d5005e22b38866b0e3cbc8d686a46
Azraf Al Monzim <dev@monzim.com>
2024-02-04 19:24:09 +0600

    Fix Harpoon keybindings and update lazy.nvim commit

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -36,9 +36,7 @@ I hope you enjoy your Neovim journey,
 - TJ
 
 P.S. You can delete this when you're done too. It's your config now :)
---]]
-
--- Set <space> as the leader key
+--]] -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
@@ -59,6 +57,9 @@ require 'keymaps'
 -- [[ Configure Telescope ]]
 -- (fuzzy finder)
 require 'telescope-setup'
+
+-- [[ Configure Harpoon ]]
+require 'harpoon-setup'
 
 -- [[ Configure Treesitter ]]
 -- (syntax parser for highlighting)
